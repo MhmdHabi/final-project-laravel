052731c1a4c8155f6e9cd0f1cff10fd33f4070fb
Fathin Fayyadh <fathinfayyadh4@gmail.com>
2024-06-17 10:53:56 +0700

    update pdf

--- app/Http/Controllers/Mahasiswa/KrsController.php
+++ app/Http/Controllers/Mahasiswa/KrsController.php
@@ -7,10 +7,11 @@ use App\Models\Krs;
 use App\Models\MatkulKrs;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class KrsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $mahasiswaId = Auth::user()->id;
 
@@ -22,6 +23,23 @@ class KrsController extends Controller
             $krs = MatkulKrs::where('krs_id', $latestKrs->id)->get();
         }
 
+        // Jika permintaan untuk export PDF, maka persiapkan data untuk ditampilkan dalam PDF
+        if ($request->get('export') == 'pdf') {
+            // Ambil data yang diperlukan untuk PDF
+            $data = [
+                'krs' => $krs,
+                'latestKrs' => $latestKrs,
+                'mahasiswaId' => $mahasiswaId,
+            ];
+
+            // Load view 'khsmahasiswa.blade.php' dengan data yang diperlukan
+            $pdf = Pdf::loadView('dashboard.mahasiswa.pdf.khsmahasiswa', $data);
+
+            // Unduh file PDF dengan nama 'krs.pdf'
+            return $pdf->stream('krs.pdf');
+        }
+
+        // Jika bukan permintaan untuk export PDF, tampilkan halaman krs.blade.php dengan data krs
         return view('dashboard.mahasiswa.krs', compact('krs'));
     }
 }
